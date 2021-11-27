7c1e40d08dfbb95d76d823ce378e91d2f23f5794
Karthick Vasudevan <karthick.tungsten@gmail.com>
2021-11-28 01:28:52 +0530

    ui implementation

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiSerivceService } from '../common-services/api-serivce.service';
+import { CommonMethodsService } from '../common-services/common-methods.service';
+import { ToastComponent } from '../popup-toast/toast/toast.component';
+declare var $:any;
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiSerivce: ApiSerivceService,private common:CommonMethodsService) { }
 
   ngOnInit(): void {
+  }
+
+  login(body: any) {
+    this.apiSerivce.postApiRequest("/login", body).subscribe(
+      {
+        next: (resp) => {
+          if (resp.status == 200) {
+            let token: any = resp.headers.get("Authorization");
+            this.common.storeToLocal("auth",token);
+          }
+        },
+        error:(err)=>{
+          
+        }
+      }
+    )
   }
 
 }
