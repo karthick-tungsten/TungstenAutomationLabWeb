6c34fb373a71f0a0bbfea6f3a2d1383802444240
Karthick Vasudevan <karthick.tungsten@gmail.com>
2021-12-08 04:30:29 +0530

    login partial complete

--- src/app/common-services/common-methods.service.ts
+++ src/app/common-services/common-methods.service.ts
@@ -1,32 +1,46 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { VariableShareService } from './variable-share.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonMethodsService {
 
-  constructor() { }
+  constructor(
+    private router:Router,
+    private vShare:VariableShareService) { }
 
-  public storeToLocal(key:any,value:any){
-    localStorage.setItem(key,value);
+  public validateToken(){
+    if(this.getFromLocal("auth")==null){
+      localStorage.clear()
+      sessionStorage.clear()
+      this.router.navigate([""])
+      let data = { "title": "Error", "message": "Please login", "type": "failure" }
+      this.vShare.showToastValues(data)
+    }
   }
 
-  public getFromLocal(key:any){
+  public storeToLocal(key: any, value: any) {
+    localStorage.setItem(key, value);
+  }
+
+  public getFromLocal(key: any) {
     return localStorage.getItem(key)
   }
-  public removeLocal(key:any){
+  public removeLocal(key: any) {
     localStorage.removeItem(key)
   }
 
-  public storeToSession(key:any,value:any){
-    sessionStorage.setItem(key,value);
+  public storeToSession(key: any, value: any) {
+    sessionStorage.setItem(key, value);
   }
 
-  public getFromSession(key:any){
+  public getFromSession(key: any): any {
     return sessionStorage.getItem(key)
   }
 
-  public removeFromSession(key:any){
+  public removeFromSession(key: any) {
     sessionStorage.removeItem(key);
   }
 }
