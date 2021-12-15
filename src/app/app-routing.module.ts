a8fa777eece22efde85b23b343d355e235a5b8e2
Karthick Vasudevan <karthick.vasudevan@indiumsoft.com>
2021-12-15 22:46:57 +0530

    routing module structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProjectDetailsComponent } from './dashboard/admin-users/project-details/project-details.component';
-import { UserDetailsComponent } from './dashboard/admin-users/user-details/user-details.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { ProjectDetailsComponent } from './main-components/dashboard/admin-users/project-details/project-details.component';
+import { UserDetailsComponent } from './main-components/dashboard/admin-users/user-details/user-details.component';
+import { DashboardComponent } from './main-components/dashboard/dashboard.component';
+import { HomePageComponent } from './main-components/home-page/home-page.component';
 
 const routes: Routes = [
   { path: "", component: HomePageComponent },
